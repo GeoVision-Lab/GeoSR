2cf0250724db637c8febcc6087b3f11855a3263c
郭 直霊 <kaku@kakunaohis-MacBook-puro.local>
2018-10-16 09:23:13 +0900

    main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@CreateTime:   2018-10-10T12:16:31+09:00
+@Email:  guozhilingty@gmail.com
+@Copyright: Chokurei
+@License: MIT
+
+Geosr - A Computer Vision Package for Remote Sensing Image Super Resolution
+"""
+from __future__ import print_function
+import argparse
+from math import log10
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+#from model import Net
+from data import get_training_set, get_test_set
+
+# Training settings
+parser = argparse.ArgumentParser(description='PyTorch Super Res Example')
+parser.add_argument('--upscale_factor', type=int, required=True, help="super resolution upscale factor")
+parser.add_argument('--batchSize', type=int, default=64, help='training batch size')
+parser.add_argument('--testBatchSize', type=int, default=10, help='testing batch size')
+parser.add_argument('--nEpochs', type=int, default=2, help='number of epochs to train for')
+parser.add_argument('--lr', type=float, default=0.01, help='Learning Rate. Default=0.01')
+parser.add_argument('--cuda', action='store_true', help='use cuda?')
+parser.add_argument('--threads', type=int, default=4, help='number of threads for data loader to use')
+parser.add_argument('--seed', type=int, default=123, help='random seed to use. Default=123')
+opt = parser.parse_args()
+
+print(opt)
+
+if opt.cuda and not torch.cuda.is_available():
+    raise Exception("No GPU found, please run without --cuda")
+    
+torch.manual_seed(opt.seed)
+
+device = torch.device("cuda" if opt.cuda else "cup")
+    
+
