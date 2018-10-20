2d118c383104dd33357d5f3d732980ad95f855b6
kaku <chokurei>
2018-10-20 21:32:42 +0900

    loader

--- utils/loader.py
+++ utils/loader.py
@@ -6,7 +6,8 @@
 @Copyright: Chokurei
 @License: MIT
 
-Load training, test, and validation data from dataset/data_dir
+Load training, test, and validation data from dataset/data_dir, \
+    with data augmentation method in DataAug
 """
 import os
 import sys
@@ -14,33 +15,11 @@ sys.path.append('./utils')
 import torch.utils.data as data
 import argparse
 from PIL import Image
-from preprocessor import input_transform, target_transform
+from utils.preprocessor import input_transform, target_transform
+from utils.preprocessor_aug import DataAug
+import torchvision.transforms.functional as TF
 
 Utils_DIR = os.path.dirname(os.path.abspath(__file__))
-
-class DatasetFromFolder(data.Dataset):
-    def __init__(self, image_dir, input_transform=None, target_transform=None):
-        super(DatasetFromFolder, self).__init__()
-        self.image_filenames = [os.path.join(image_dir, x) for x in os.listdir(image_dir) if is_image_file(x)]
-
-        self.input_transform = input_transform
-        self.target_transform = target_transform
-
-    def __getitem__(self, index):
-        input = load_img(self.image_filenames[index])
-        target = input.copy()
-        if self.input_transform:
-            input = self.input_transform(input)
-        if self.target_transform:
-            target = self.target_transform(target)
-
-        return input, target
-
-    def __len__(self):
-        return len(self.image_filenames)
-
-def is_image_file(filename):
-    return any(filename.endswith(extension) for extension in [".png", ".jpg", ".jpeg", ".tif"])
 
 def load_img(filepath):
     """
@@ -52,48 +31,94 @@ def load_img(filepath):
     y = img
     return y
 
+class DatasetFromFolder(data.Dataset):
+    def __init__(self, image_dir, transform=True, aug_mode='a', crop_size=224, upscale_factor=2):
+        super(DatasetFromFolder, self).__init__()
+        self.image_filenames = [os.path.join(image_dir, x) for x in os.listdir(image_dir) if is_image_file(x)]
+        self.transform = transform
+        self.trans_mode = aug_mode
+        self.crop_size = crop_size
+        self.upscale_factor = upscale_factor
+
+    def __getitem__(self, index):
+        """
+        data.Dataset can make index range from 0 to len(image_filenames)
+        """
+        input = load_img(self.image_filenames[index])
+        target = input.copy()
+        if self.transform:
+#            input, target = DataAug(input, target, self.crop_size, self.upscale_factor)
+            data_aug = DataAug(input, target, self.trans_mode, self.crop_size, self.upscale_factor)
+            input, target = data_aug()
+        else:
+            input = TF.center_crop(input, (self.crop_size, self.crop_size))
+            input = TF.resize(input, (self.crop_size // self.upscale_factor, self.crop_size // self.upscale_factor))
+            input = TF.to_tensor(input)
+
+            target = TF.center_crop(target, (self.crop_size, self.crop_size))
+            target = TF.to_tensor(target)
+
+        return input, target
+
+    def __len__(self):
+        return len(self.image_filenames)
+
+def is_image_file(filename):
+    return any(filename.endswith(extension) for extension in [".png", ".jpg", ".jpeg", ".tif"])
+
 def calculate_valid_crop_size(crop_size, upscale_factor):
     return crop_size - (crop_size % upscale_factor)
 
-def get_training_set(data_dir, crop_size, upscale_factor):
-    root_dir = os.path.join(data_dir,'image')
+def get_training_set(data_dir, aug, aug_mode, crop_size, upscale_factor):
+    root_dir = os.path.join(data_dir,'images')
     train_dir = os.path.join(root_dir, "train")
     crop_size = calculate_valid_crop_size(crop_size, upscale_factor)
 
     return DatasetFromFolder(train_dir,
-                             input_transform=input_transform(crop_size, upscale_factor),
-                             target_transform=target_transform(crop_size))
+                             transform=aug,
+                             aug_mode=aug_mode,
+                             crop_size=crop_size, 
+                             upscale_factor=upscale_factor)
 
-def get_val_set(data_dir, crop_size, upscale_factor):
-    root_dir = os.path.join(data_dir,'image')
+def get_val_set(data_dir, aug, aug_mode, crop_size, upscale_factor):
+    root_dir = os.path.join(data_dir,'images')
     val_dir = os.path.join(root_dir, "val")
     crop_size = calculate_valid_crop_size(crop_size, upscale_factor)
 
     return DatasetFromFolder(val_dir,
-                             input_transform=input_transform(crop_size, upscale_factor),
-                             target_transform=target_transform(crop_size))
+                             transform=aug,
+                             aug_mode=aug_mode,
+                             crop_size=crop_size, 
+                             upscale_factor=upscale_factor)
 
-def get_test_set(data_dir, crop_size, upscale_factor):
-    root_dir = os.path.join(data_dir,'image')
+def get_test_set(data_dir, aug, aug_mode, crop_size, upscale_factor):
+    root_dir = os.path.join(data_dir,'images')
     test_dir = os.path.join(root_dir, "test")
     crop_size = calculate_valid_crop_size(crop_size, upscale_factor)
 
     return DatasetFromFolder(test_dir,
-                             input_transform=input_transform(crop_size, upscale_factor),
-                             target_transform=target_transform(crop_size))
+                             transform=aug,
+                             aug_mode=aug_mode,
+                             crop_size=crop_size, 
+                             upscale_factor=upscale_factor)
+
 if __name__ == "__main__":
+
     parser = argparse.ArgumentParser(description='PyTorch Super Res Example')
     parser.add_argument('--data_dir', type=str, default=os.path.join(Utils_DIR, '../dataset','map-rand'), help="data directory")
     parser.add_argument('--crop_size', type=int, default=224, help='crop size from each data. Default=224 (same to image size)')
     parser.add_argument('--upscale_factor', type=int, default=2, help="super resolution upscale factor")
+    parser.add_argument('--aug', type=lambda x: (str(x).lower() == 'true'), default=True, help='data augmentation or not') 
+    parser.add_argument('--aug_mode', type=str, default='c', choices=['a', 'b', 'c', 'd', 'e'], 
+                        help='data augmentation mode: a, b, c, d, e')
     opt = parser.parse_args()
     print(opt)
     
-    train_set = get_training_set(opt.data_dir, opt.crop_size, opt.upscale_factor)
+    train_set = get_training_set(opt.data_dir, opt.aug, opt.aug_mode,opt.crop_size, opt.upscale_factor)
     print('Training number: {}'.format(len(train_set.image_filenames)))
-    print('Input transform:')
-    print(train_set.input_transform)
-    print('Target transform:')
-    print(train_set.target_transform)
+#    print('Input transform:')
+#    print(train_set.input_transform)
+#    print('Target transform:')
+#    print(train_set.target_transform)
 
 
--- utils/preprocessor.py
+++ utils/preprocessor.py
@@ -7,24 +7,143 @@
 @License: MIT
 
 preprocessor.py
-    Data augmentation
+    Scalable data augmentation methods 
 """
 import os
 import sys
 sys.path.append('./utils')
-from torchvision.transforms import Compose, CenterCrop, ToTensor, Resize, FiveCrop
+from torchvision.transforms import Compose, CenterCrop, ToTensor, Resize, FiveCrop, RandomCrop
+import torchvision.transforms.functional as TF
+import torch.utils.data as data
+import random
+from PIL import Image
 
 Utils_DIR = os.path.dirname(os.path.abspath(__file__))
 
-def input_transform(crop_size, upscale_factor):
-    return Compose([
-        CenterCrop(crop_size),
-#        FiveCrop(crop_size),
-        Resize(crop_size // upscale_factor),
-        ToTensor(),])
+class DataAug(object):
+    """
+    Scalable data augmentation methods
+    Add self augmentation method at the end
+    """
+    def __init__(self, image, mask, trans_mode, crop_size, upscale_factor):
+        
+        self.image = image
+        self.mask = mask
+        self.trans_mode = trans_mode
+        self.crop_size = crop_size
+        self.upscale_factor = upscale_factor
 
-def target_transform(crop_size):
-    return Compose([
-        CenterCrop(crop_size),
-        ToTensor(),])
+    def transform_a(self, image, mask):
+        """
+        Center crop
+        """
+        # Center crop
+        image = TF.center_crop(image, (self.crop_size,self.crop_size))
+        mask = TF.center_crop(image, (self.crop_size,self.crop_size))
+        
+        # resize
+        image = image
+        image = TF.resize(image, (self.crop_size // self.upscale_factor,self.crop_size // self.upscale_factor))
+        
+        # Transform to tensor
+        image = TF.to_tensor(image)
+        mask = TF.to_tensor(mask)
+        return image, mask
+    
+    def transform_b(self, image, mask):
+        """
+        Center crop + Random horizontal flipping
+        """        
+        # Center crop
+        image = TF.center_crop(image, (self.crop_size,self.crop_size))
+        mask = TF.center_crop(image, (self.crop_size,self.crop_size))
+        
+        # resize
+        image = image
+        image = TF.resize(image, (self.crop_size // self.upscale_factor,self.crop_size // self.upscale_factor))
+        
+        # Random horizontal flipping
+        if random.random() > 0.5:
+            image = TF.hflip(image)
+            mask = TF.hflip(mask)
+        
+        # Transform to tensor
+        image = TF.to_tensor(image)
+        mask = TF.to_tensor(mask)
+        return image, mask
+    
+    def transform_c(self, image, mask):
+        """
+        Center crop + Random horizontal flipping + Random vertical flipping
+        """        
+        # Center crop
+        image = TF.center_crop(image, (self.crop_size,self.crop_size))
+        mask = TF.center_crop(image, (self.crop_size,self.crop_size))
+        
+        # resize
+        image = image
+        image = TF.resize(image, (self.crop_size // self.upscale_factor,self.crop_size // self.upscale_factor))
+        
+        # Random horizontal flipping
+        if random.random() > 0.5:
+            image = TF.hflip(image)
+            mask = TF.hflip(mask)
+
+        # Random vertical flipping
+        if random.random() > 0.5:
+            image = TF.vflip(image)
+            mask = TF.vflip(mask)
+        
+        # Transform to tensor
+        image = TF.to_tensor(image)
+        mask = TF.to_tensor(mask)
+        return image, mask
+
+    def transform_d(self, image, mask):
+        """
+        Random crop + Random horizontal flipping + Random vertical flipping
+        """  
+        # Random crop
+        i, j, h, w = RandomCrop.get_params(
+            image, output_size=(self.crop_size, self.crop_size))
+        image = TF.crop(image, i, j, h, w)
+        mask = TF.crop(mask, i, j, h, w)
+        
+        # resize
+        image = image
+        image = TF.resize(image, (self.crop_size // self.upscale_factor,self.crop_size // self.upscale_factor))
+        
+        # Random horizontal flipping
+        if random.random() > 0.5:
+            image = TF.hflip(image)
+            mask = TF.hflip(mask)
+
+        # Random vertical flipping
+        if random.random() > 0.5:
+            image = TF.vflip(image)
+            mask = TF.vflip(mask)
+        
+        # Transform to tensor
+        image = TF.to_tensor(image)
+        mask = TF.to_tensor(mask)
+        return image, mask
+
+    def transform_e(self, image, mask):
+        """
+        Deside by your self
+        """  
+        raise TypeError('Please decide your own augmentation method')
+    
+    def __call__(self):
+        if self.trans_mode == 'a':
+            image, mask = self.transform_a(self.image, self.mask)
+        if self.trans_mode == 'b':
+            image, mask = self.transform_b(self.image, self.mask)
+        if self.trans_mode == 'c':
+            image, mask = self.transform_c(self.image, self.mask)
+        if self.trans_mode == 'd':
+            image, mask = self.transform_d(self.image, self.mask)
+        if self.trans_mode == 'e':
+            image, mask = self.transform_e(self.image, self.mask)
+        return image, mask
 
