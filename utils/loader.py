4ab9fc58d8e14a7d25fa46b7404e7cdf2d7cfdc4
kaku <chokurei>
2018-10-25 15:34:20 +0900

    update

--- utils/loader.py
+++ utils/loader.py
@@ -11,14 +11,19 @@ Load training, test, and validation data from dataset/data_dir, \
 """
 import os
 import sys
-sys.path.append('./utils')
+Utils_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join('..', Utils_DIR))
+#sys.path.insert(0, os.path.abspath(".."))
 import torch.utils.data as data
 import argparse
 from PIL import Image
-from utils.preprocessor import DataAug
+from preprocessor import DataAug
 import torchvision.transforms.functional as TF
 
-Utils_DIR = os.path.dirname(os.path.abspath(__file__))
+import warnings
+warnings.filterwarnings("ignore", message="numpy.dtype size changed")
+warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
+
 
 def load_img(filepath):
     """
@@ -41,14 +46,15 @@ class DatasetFromFolder(data.Dataset):
 
     def __getitem__(self, index):
         """
-        data.Dataset can make index range from 0 to len(image_filenames)
+        data.Dataset can make index range from 0 to len(image_filenames) iteratively
         """
         input = load_img(self.image_filenames[index])
         target = input.copy()
         if self.transform:
-#            input, target = DataAug(input, target, self.crop_size, self.upscale_factor)
             data_aug = DataAug(input, target, self.trans_mode, self.crop_size, self.upscale_factor)
             input, target = data_aug()
+            # input: torch.Size([nb_channel, crop_size//upscale_factor, crop_size//upscale_factor])
+            # target: torch.Size([nb_channel, crop_size, crop_size])
         else:
             input = TF.center_crop(input, (self.crop_size, self.crop_size))
             input = TF.resize(input, (self.crop_size // self.upscale_factor, self.crop_size // self.upscale_factor))
@@ -56,7 +62,6 @@ class DatasetFromFolder(data.Dataset):
 
             target = TF.center_crop(target, (self.crop_size, self.crop_size))
             target = TF.to_tensor(target)
-
         return input, target
 
     def __len__(self):
@@ -102,19 +107,21 @@ def get_test_set(data_dir, aug, aug_mode, crop_size, upscale_factor):
                              upscale_factor=upscale_factor)
 
 if __name__ == "__main__":
+    print('Load training, test, and validation data from dataset/data_dir, \
+    with data augmentation method in DataAug')
 
-    parser = argparse.ArgumentParser(description='PyTorch Super Res Example')
-    parser.add_argument('--data_dir', type=str, default=os.path.join(Utils_DIR, '../dataset','map-rand'), help="data directory")
-    parser.add_argument('--crop_size', type=int, default=224, help='crop size from each data. Default=224 (same to image size)')
-    parser.add_argument('--upscale_factor', type=int, default=2, help="super resolution upscale factor")
-    parser.add_argument('--aug', type=lambda x: (str(x).lower() == 'true'), default=True, help='data augmentation or not') 
-    parser.add_argument('--aug_mode', type=str, default='c', choices=['a', 'b', 'c', 'd', 'e'], 
-                        help='data augmentation mode: a, b, c, d, e')
-    opt = parser.parse_args()
-    print(opt)
-    
-    train_set = get_training_set(opt.data_dir, opt.aug, opt.aug_mode,opt.crop_size, opt.upscale_factor)
-    print('Training number: {}'.format(len(train_set.image_filenames)))
+#    parser = argparse.ArgumentParser(description='PyTorch Super Res Example')
+#    parser.add_argument('--data_dir', type=str, default=os.path.join(Utils_DIR, '../dataset','map-rand'), help="data directory")
+#    parser.add_argument('--crop_size', type=int, default=224, help='crop size from each data. Default=224 (same to image size)')
+#    parser.add_argument('--upscale_factor', type=int, default=2, help="super resolution upscale factor")
+#    parser.add_argument('--aug', type=lambda x: (str(x).lower() == 'true'), default=True, help='data augmentation or not') 
+#    parser.add_argument('--aug_mode', type=str, default='c', choices=['a', 'b', 'c', 'd', 'e'], 
+#                        help='data augmentation mode: a, b, c, d, e')
+#    opt = parser.parse_args()
+#    print(opt)
+#    
+#    train_set = get_training_set(opt.data_dir, opt.aug, opt.aug_mode,opt.crop_size, opt.upscale_factor)
+#    print('Training number: {}'.format(len(train_set.image_filenames)))
 #    print('Input transform:')
 #    print(train_set.input_transform)
 #    print('Target transform:')
